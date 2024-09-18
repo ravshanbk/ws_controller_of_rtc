6f541ebc750cb8645b8698afe3a6168109fbeee4
ravshanbk <88109580+ravshanbk@users.noreply.github.com>
2024-09-18 13:47:31 +0500

    add to show ConnectionState

--- lib/ws_controller_of_rtc.dart
+++ lib/ws_controller_of_rtc.dart
@@ -2,5 +2,5 @@
 library;
 
 export 'package:web_socket_client_rtc_test/web_socket_client.dart'
-    show Connected, Reconnected;
+    show Connected, ConnectionState ,Reconnected;
 export 'package:ws_controller_of_rtc/src/counter_repository.dart';
